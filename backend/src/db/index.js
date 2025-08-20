9470892a766a967845e1e76c5c9213c5904e687e
Ankit Gupta <guptankit.2003@gmail.com>
2025-08-20 23:34:13 +0530

    Add dependencies and configuration for frontend and backend
    - Updated package.json and package-lock.json to include axios, lucide-react, react-hot-toast, react-icons, react-router-dom, zod, zustand, and prettier in the frontend.
    - Implemented MongoDB connection logic in backend index.js.
    - Created custom error handling classes ApiError and ApiResponse.
    - Added asyncHandler utility for handling asynchronous requests.
    - Configured Cloudinary for file uploads in cloudinary.js.
    - Added .prettierignore and .prettierrc for code formatting settings.

---
+++ backend/src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env",
+});
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGO_URL}`
+        );
+        console.log(
+            `/n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`
+        );
+    } catch (err) {
+        console.log(`MongoDB connection error `, err);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
