fd9e43840dad8d994f69600cd426559acbb56932
Ankit Gupta <guptankit.2003@gmail.com>
2025-08-22 12:41:52 +0530

    feat: add authentication methods and environment configuration for admin users

--- backend/src/models/adminUser.model.js
+++ backend/src/models/adminUser.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const ROLES = ["owner", "admin", "editor"];
 const STATUSES = ["active", "disabled"];
@@ -23,5 +26,51 @@ const adminUserSchema = new Schema(
 );
 
 adminUserSchema.index({ email: 1 }, { unique: true });
+
+adminUserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+adminUserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+adminUserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
+
+adminUserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
+};
+
+adminUserSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20min
+
+  return { hashedToken, unHashedToken, tokenExpiry };
+};
 
 export const AdminUser = model("AdminUser", adminUserSchema);
